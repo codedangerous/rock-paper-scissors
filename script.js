9e01e84f43557f1b145241512e0bec9df761f298
codedangerous <cody.robinett@gmail.com>
2025-07-28 12:30:34 +0000

    Add getComputerChoice function
    Added a function that generates a random number
    between 1 and 3 and uses that to return "rock",
    "paper", or "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+function getComputerChoice() {
+    let randomNum = Math.floor((Math.random() * 3)) + 1;
+    
+    if (randomNum === 1) {
+        return "rock";
+    }
+    else if (randomNum === 2) {
+        return "paper";
+    }
+    else if (randomNum === 3) {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
